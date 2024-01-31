b1d271ad84de62d6f77b23a502f5d658c13f2f6a
g1ibby <me@appci.xyz>
2024-01-31 13:18:15 +0700

    init chainlit

---
+++ app.py
@@ -0,0 +1,38 @@
+import chainlit as cl
+from chainlit.input_widget import Select, Switch, Slider
+
+@cl.on_chat_start
+async def start():
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="Model",
+                label="OpenAI - Model",
+                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
+                initial_index=0,
+            ),
+            Switch(id="Streaming", label="OpenAI - Stream Tokens", initial=True),
+            Slider(
+                id="Temperature",
+                label="OpenAI - Temperature",
+                initial=1,
+                min=0,
+                max=2,
+                step=0.1,
+            ),
+        ]
+    ).send()
+
+@cl.on_settings_update
+async def setup_agent(settings):
+    print("on_settings_update", settings)
+
+@cl.on_message
+async def main(message: cl.Message):
+    # Your custom logic goes here...
+
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {message.content}",
+    ).send()
+
